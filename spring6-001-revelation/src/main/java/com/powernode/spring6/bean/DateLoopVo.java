c201cd6b16b77632af50aef074ab6515791cc367
chill <1535726475@qq.com>
2023-04-21 13:21:30 +0800

    04-21

--- spring6-001-revelation/src/main/java/com/powernode/spring6/bean/DateLoopVo.java
+++ spring6-001-revelation/src/main/java/com/powernode/spring6/bean/DateLoopVo.java
@@ -25,4 +25,5 @@ public class DateLoopVo {
 
     public DateLoopVo() {
     }
+    
 }
--- spring6-001-revelation/src/test/java/T/DateTableGenerator.java
+++ spring6-001-revelation/src/test/java/T/DateTableGenerator.java
@@ -9,49 +9,45 @@ import java.util.List;
 
 public class DateTableGenerator {
     public static void main(String[] args) {
-        DateLoopVo dateLoopVo1 =
+        DateLoopVo dateLoopVo001 =
                 new DateLoopVo("abc",LocalDate.parse("2023-03-05",DateTimeFormatter.ISO_DATE),LocalDate.parse("2024-03-05",DateTimeFormatter.ISO_DATE));
-        DateLoopVo dateLoopVo2 =
+        DateLoopVo dateLoopVo002 =
                 new DateLoopVo("abc123",LocalDate.parse("2021-03-05",DateTimeFormatter.ISO_DATE),LocalDate.parse("2022-03-05",DateTimeFormatter.ISO_DATE));
         List<DateLoopVo>  dataLoops = new ArrayList<>();
-        dataLoops.add(dateLoopVo1);
-        dataLoops.add(dateLoopVo2);
-
-        dataLoops.forEach(item->System.out.println(item));
-        System.out.println("---------------");
+        dataLoops.add(dateLoopVo001);
+        dataLoops.add(dateLoopVo002);
 
 
-        String assetCode = dateLoopVo1.getAssetCode();
-        LocalDate startDate = dateLoopVo1.getStartDate();
-        LocalDate endDate = dateLoopVo1.getEndDate();
-        ArrayList<ArrayList<String>> dateTable = new ArrayList<>();
-        ArrayList<String> headerRow = new ArrayList<>();
-        headerRow.add("Asset Code");
-        headerRow.add("Start Date");
-        headerRow.add("Next Date");
-        dateTable.add(headerRow);
-        LocalDate currentDate = startDate;
+        for(DateLoopVo dateLoopVo1 : dataLoops){
+            //List<DateLoopVo> cars = new ArrayList<>();
+            DateLoopVo car = new DateLoopVo();
 
-        //循环写入
-        for (int i = 0; !currentDate.isAfter(endDate); i++) {
-            ArrayList<String> currentRow = new ArrayList<>();
-            currentRow.add(assetCode);
-            currentRow.add(currentDate.format(DateTimeFormatter.ISO_DATE));
-            if (i == 0) {
-                currentRow.add(currentDate.plusDays(45).format(DateTimeFormatter.ISO_DATE));
-                currentDate = currentDate.plusDays(45);
-            } else {
-                currentRow.add(currentDate.plusDays(30).format(DateTimeFormatter.ISO_DATE));
-                currentDate = currentDate.plusDays(30);
+            //获取数据
+            String assetCode = dateLoopVo1.getAssetCode();
+            LocalDate startDate = dateLoopVo1.getStartDate();
+            LocalDate endDate = dateLoopVo1.getEndDate();
+
+            LocalDate currentDate = startDate;
+            //循环写入
+            for (int i = 0; !currentDate.isAfter(endDate); i++) {
+                car.setAssetCode(assetCode);
+                //首次循环取start_date否则取上条end_date+1
+                if(i == 0){
+                    car.setStartDate(currentDate);
+                }
+                car.setStartDate(currentDate.plusDays(1));
+
+                if (i == 0) {
+                    car.setEndDate(currentDate.plusDays(45));
+                    currentDate = currentDate.plusDays(45);
+                } else {
+                    car.setEndDate(currentDate.plusDays(30));
+                    currentDate = currentDate.plusDays(30);
+                }
+                //cars.add(car);
+                System.out.println(car);
             }
-            dateTable.add(currentRow);
         }
-        //将数据写入表
-        for (ArrayList<String> row : dateTable) {
-            for (String cell : row) {
-                System.out.print(cell + "\t");
-            }
-            System.out.println();
-        }
+
     }
 }
